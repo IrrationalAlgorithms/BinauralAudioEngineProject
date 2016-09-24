526d7e57216ae46bb9f13297216914fc281b0e65
Kanoka <bars9@inbox.ru>
2016-09-24 11:16:17 +0300

    first commit

---
+++ Mixer/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.DirectX.DirectSound;
+using Microsoft.DirectX.Direct3D;
+using System.Windows.Interop;
+using System.Windows.Forms;
+
+namespace Mixer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += new RoutedEventHandler(Window_Loaded);
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            RoutedCommand dowmCommand = new RoutedCommand();
+            dowmCommand.InputGestures.Add(new KeyGesture(Key.Down));
+            CommandBindings.Add(new CommandBinding(dowmCommand, downCommandEvent));
+        }
+
+        private void downCommandEvent(object sender, RoutedEventArgs e)
+        {
+            //handler code goes here. 
+            new SoundGenerator.SineGenerator().Generate();
+        }
+    }
+}
---
+++ SoundGenerator/SineGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Threading;
+using SharpDX;
+using SharpDX.Multimedia;
+using SharpDX.XAudio2;
+
+namespace SoundGenerator
+{
+    public class SineGenerator
+    {
+        public void Generate()
+        {
+            var xaudio2 = new XAudio2();
+            var masteringVoice = new MasteringVoice(xaudio2);
+
+            var waveFormat = new WaveFormat(44100, 32, 2);
+            var sourceVoice = new SourceVoice(xaudio2, waveFormat);
+
+            int bufferSize = waveFormat.ConvertLatencyToByteSize(60000);
+            var dataStream = new DataStream(bufferSize, true, true);
+
+            int numberOfSamples = bufferSize / waveFormat.BlockAlign;
+            for (int i = 0; i < numberOfSamples; i++)
+            {
+                float value = (float)(Math.Cos(2 * Math.PI * (220.0 + 4.0) * i / waveFormat.SampleRate) * 0.5);
+                dataStream.Write(value);
+                dataStream.Write(value);
+            }
+            dataStream.Position = 0;
+
+            var audioBuffer = new AudioBuffer { Stream = dataStream, Flags = BufferFlags.EndOfStream, AudioBytes = bufferSize };
+
+            sourceVoice.SubmitSourceBuffer(audioBuffer, null);
+
+            sourceVoice.Start();
+
+            Console.WriteLine("Play sound");
+            for (int i = 0; i < 60; i++)
+            {
+                Console.Write(".");
+                Console.Out.Flush();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}

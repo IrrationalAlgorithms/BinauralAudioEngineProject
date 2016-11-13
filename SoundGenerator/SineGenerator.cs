4b651a11dd2cebb11178ffb3f547aaeab917a6a7
Ba-Ski <morbius95@mail.ru>
2016-11-13 17:00:49 +0300

    added linera changing of sound to SineGenerator

--- SoundGenerator/SineGenerator.cs
+++ SoundGenerator/SineGenerator.cs
@@ -25,10 +25,19 @@ namespace SoundGenerator
         private int _nextBuffer;
         private ManualResetEventSlim _playEvent;
 
+        private bool _isConvolutionOn;
+        private int _horizontalAngle;
+        private int _elevation;
+
         private List<string> _log;
 
         private int _valueRate;
         private float _valueAmp;
+
+        private const int MAX_HORIZONATL_ANGLE_ABS = 180;
+        private const int HORIZONTAL_ANGLE_RANGE = 360;
+        private const int MAX_ELEVATION_ANGLE = 90;
+        private const int MIN_ELEVATION_ANGLE = -40;
 
         public SineGenerator()
         {
@@ -45,6 +54,10 @@ namespace SoundGenerator
             _sourceVoice.BufferEnd += sourceVoice_BufferEnd;
             _sourceVoice.Start();
 
+            _isConvolutionOn = false;
+            _horizontalAngle = 0;
+            _elevation = 0;
+
             _bufferEndEvent = new AutoResetEvent(false);
 
             _valueRate = 0;
@@ -54,6 +67,7 @@ namespace SoundGenerator
 
             _playEvent = new ManualResetEventSlim();
             _log = new List<string>();
+
 
             // Pre-allocate buffers
             _audioBuffersRing = new AudioBuffer[3];
@@ -76,6 +90,31 @@ namespace SoundGenerator
         public void AddValueAmp(float value)
         {
             _valueAmp += value;
+        }
+
+        public void SetConvolutionMode(bool isOn)
+        {
+            _isConvolutionOn = isOn;
+        }
+
+        public void SetHorizontalPosition(int angle)
+        {
+            _horizontalAngle = angle % MAX_HORIZONATL_ANGLE_ABS;
+        }
+
+        public void SetElevation(int angle)
+        {
+            if (angle < MIN_ELEVATION_ANGLE || angle > 180 + MIN_ELEVATION_ANGLE)
+            {
+                _elevation = MIN_ELEVATION_ANGLE;
+            } else if (angle > 90 && angle <= 180 + MIN_ELEVATION_ANGLE)
+            {
+                _elevation = MAX_ELEVATION_ANGLE - (angle - MAX_ELEVATION_ANGLE);
+            }
+            else
+            {
+                _elevation = angle;
+            }
         }
 
         public void Play()
@@ -101,49 +140,60 @@ namespace SoundGenerator
             try
             {
                 int numberOfSamples = _bufferSize / _waveFormat.BlockAlign;
-
+                
                 while (true)
                 {
-                    while (_playerState != PlayerState.Stopped)
+                    if (_playerState != PlayerState.Stopped)
                     {
-                        if (_playEvent.Wait(_waitPrecision))
-                            break;
+                        while (_playerState != PlayerState.Stopped)
+                        {
+                            if (_playEvent.Wait(_waitPrecision))
+                                break;
+                        }
+
+                        for (int i = 0; i < numberOfSamples; i++)
+                        {
+                            float value = (float) (Math.Sin(2*Math.PI*_valueRate*i/_waveFormat.SampleRate)*_valueAmp);
+                            _dataStream.Write(value);
+                            _dataStream.Write(value);
+                        }
+
+                        _dataStream.Position = 0;
+
+                        while (_sourceVoice.State.BuffersQueued == _audioBuffersRing.Length)
+                            _bufferEndEvent.WaitOne(_waitPrecision);
+
+                        if (_bufferSize > _memBuffers[_nextBuffer].Size)
+                        {
+                            if (_memBuffers[_nextBuffer].Pointer != IntPtr.Zero)
+                                Utilities.FreeMemory(_memBuffers[_nextBuffer].Pointer);
+
+                            _memBuffers[_nextBuffer].Pointer = Utilities.AllocateMemory(_bufferSize);
+                            _memBuffers[_nextBuffer].Size = _bufferSize;
+                        }
+
+                        // Copy the memory from MediaFoundation AudioDecoder to the buffer that is going to be played.
+                        Utilities.CopyMemory(_memBuffers[_nextBuffer].Pointer, _dataStream.DataPointer, _bufferSize);
+
+                        // Set the pointer to the data.
+                        _audioBuffersRing[_nextBuffer].AudioDataPointer = _memBuffers[_nextBuffer].Pointer;
+                        _audioBuffersRing[_nextBuffer].AudioBytes = _bufferSize;
+
+                        if (!_isConvolutionOn)
+                        {
+                            var angle = _horizontalAngle%90;
+                            float rightChannelVolume = (float) (angle + MAX_HORIZONATL_ANGLE_ABS/2)/
+                                                       (HORIZONTAL_ANGLE_RANGE/2);
+                            float leftChannelVolume = 1 - rightChannelVolume;
+                            _sourceVoice.SetChannelVolumes(2, new[] {leftChannelVolume, rightChannelVolume});
+                        }
+
+                        _sourceVoice.SubmitSourceBuffer(_audioBuffersRing[_nextBuffer], null);
+
+                        // Go to next entry in the ringg audio buffer
+                        _nextBuffer = ++_nextBuffer%_audioBuffersRing.Length;
+
                     }
-
-                    for (int i = 0; i < numberOfSamples; i++)
-                    {
-                        float value = (float)(Math.Sin(2 * Math.PI * _valueRate * i / _waveFormat.SampleRate) * _valueAmp);
-                        _dataStream.Write(value);
-                        _dataStream.Write(value);
-                    }
-
-                    _dataStream.Position = 0;
-
-                    while (_sourceVoice.State.BuffersQueued == _audioBuffersRing.Length)
-                        _bufferEndEvent.WaitOne(_waitPrecision);
-
-                    if (_bufferSize > _memBuffers[_nextBuffer].Size)
-                    {
-                        if (_memBuffers[_nextBuffer].Pointer != IntPtr.Zero)
-                            Utilities.FreeMemory(_memBuffers[_nextBuffer].Pointer);
-
-                        _memBuffers[_nextBuffer].Pointer = Utilities.AllocateMemory(_bufferSize);
-                        _memBuffers[_nextBuffer].Size = _bufferSize;
-                    }
-
-                    // Copy the memory from MediaFoundation AudioDecoder to the buffer that is going to be played.
-                    Utilities.CopyMemory(_memBuffers[_nextBuffer].Pointer, _dataStream.DataPointer, _bufferSize);
-
-                    // Set the pointer to the data.
-                    _audioBuffersRing[_nextBuffer].AudioDataPointer = _memBuffers[_nextBuffer].Pointer;
-                    _audioBuffersRing[_nextBuffer].AudioBytes = _bufferSize;
-
-                    _sourceVoice.SubmitSourceBuffer(_audioBuffersRing[_nextBuffer], null);
-
-                    // Go to next entry in the ringg audio buffer
-                    _nextBuffer = ++_nextBuffer % _audioBuffersRing.Length;
-
-
                 }
             }
             finally
